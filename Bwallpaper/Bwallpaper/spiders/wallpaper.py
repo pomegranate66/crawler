52470db28cf28bbb2820927c9d4e7d66c3e23a49
QzWang <wqzhpu@163.com>
2019-08-19 16:41:27 +0800

    中关村壁纸爬虫

---
+++ Bwallpaper/Bwallpaper/spiders/wallpaper.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+# from Bwallpaper.Bwallpaper.input_url import Application
+
+class WallpaperSpider(scrapy.Spider):
+    name = 'wallpaper'
+    allowed_domains = ['desk.zol.com.cn']
+    url = 'http://desk.zol.com.cn'
+    x = input('请输入你要爬取的壁纸的第一页网址:')
+    start_urls = [x]
+
+    def parse(self, response):
+        image_name = response.xpath('/html/body/div[3]/h3')[0].xpath('string(.)').extract_first().replace('\r\n\t\t',
+                                                                                                          '').strip()
+        image_url = response.xpath('//img[@id="bigImg"]/@src').extract_first()
+        next_url = response.xpath('//a[@id="pageNext"]/@href').extract_first()
+        yield {
+            'image_name': image_name,
+            'image_url': image_url,
+        }
+        yield scrapy.Request(self.url + next_url, callback=self.parse)
